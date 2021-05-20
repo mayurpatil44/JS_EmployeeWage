02e39abc9f3bf7c580b9d5320b474afe372f96a9
mayurpatil44 <mayurpatil44@hotmail.com>
2021-05-20 15:19:24 +0530

    To Check if Employee Present or Absent

---
+++ EmployeeWage.js
@@ -0,0 +1,15 @@
+console.log("Welcome to Employee Wage");
+//UC1-To Check If Employee is Absent Or Present
+const  IS_ABSENT=0;
+{
+let empCheck=Math.floor(Math.random()*10)%2;
+console.log(empCheck);
+if(empCheck==IS_ABSENT)
+{
+    console.log("Employee is absent");
+}
+else
+{
+    console.log("Employee is present");
+}
+}
