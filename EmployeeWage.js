60ae2e451630d08bee53c2f02a8ded5c42c5a4fa
mayurpatil44 <mayurpatil44@hotmail.com>
2021-05-20 15:26:02 +0530

    To calculate parttime and fulltime wage for an employees

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -13,3 +13,24 @@ else
     console.log("Employee is present");
 }
 }
+//UC2-To Check PartTime and FullTime Wage
+console.log("uc2-PartTime or fulltime wage")
+const IS_Full_TIME=2;
+const IS_PART_TIME=1;
+const WAGE_PER_HOUR=20;
+let emp_hours=0;
+let checkForEmployeePresence= Math.floor((Math.random()*10)%3);
+switch(checkForEmployeePresence)
+{
+    case 1:       
+        emp_hours=4;
+        break;
+    case 2:      
+        emp_hours=8;
+        break;
+    default:
+        emp_hours=0;
+        break;
+}
+let employeeWage= WAGE_PER_HOUR*emp_hours;
+console.log("empWage : "+employeeWage);
